4a77b4cde1018ea05b06034c9381258df1e45a6e
Jong Wook Kang <50934118+timbergrizz@users.noreply.github.com>
2022-05-24 14:34:17 +0900

    Directory Edit : move ./config under src

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const app = require("express")()
 const port = 3000
-const getConnection = require("./config/db_connection.js");
+const getConnection = require("./src/config/db_connection.js");
 
 app.get("/", (req, res) =>{
     res.send("Testing");
@@ -8,10 +8,4 @@ app.get("/", (req, res) =>{
 
 app.listen(port, ()=>{
     console.log("Example app listening on 3000");
-    getConnection((conn) => {
-        conn.query("select * from movie").then(result => {
-            console.log(result)
-        })
-        conn.release();
-    })
 });
