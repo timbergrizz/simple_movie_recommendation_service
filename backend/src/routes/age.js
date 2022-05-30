4e05ed1b6479dde34ec5a6071eab21f0a352055f
Jong Wook Kang <50934118+timbergrizz@users.noreply.github.com>
2022-05-30 21:33:44 +0900

    feature : building router for top 10 movies by age

---
+++ backend/src/routes/age.js
@@ -0,0 +1,34 @@
+const router = require("express").Router()
+const getConnection = require("../config/db_connection.js");
+
+router.get("/:age", (req, res) => {
+    const age = req.params.age
+    if(age = undefined) res.send({"error" : true, "message" : "parameter does not exists."})
+
+    getConnection((conn) => {
+        conn.query(`
+            select *
+            from movie join
+            (select movieId, avg(ratingScore) as avgRate, count()
+            from ratings join
+            (select userId from user where age >= 10 and age < 20)
+            as occupation
+            on occupation.userId = ratings.userId
+            group by movieId having count() > 5) as ratings
+            on movie.movieId = ratings.movieId
+            order by avgRate desc limit 10;
+        `).then((result) => {
+            console.log(result);
+            res.send(result);
+        }).catch(err => {
+            console.log(err);
+            res.send({
+                "error" : true,
+                "message" : err
+            })
+        }).finally(() => {
+            if(conn) conn.release()
+        })
+    }
+    )
+})
