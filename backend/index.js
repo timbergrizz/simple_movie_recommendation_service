2c34a034da31fd6882e6b5dc0c71228893231476
Jong Wook Kang <50934118+timbergrizz@users.noreply.github.com>
2022-06-08 02:20:51 +0900

    Fix : port conflict with api server and react-dev

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const app = require("express")()
-const port = 3000
+const port = 3003
 const getConnection = require("./src/config/db_connection.js");
 const route = require("./src/route.js")
 
 app.use("/", route)
 
 app.listen(port, ()=>{
-    console.log("Example app listening on 3000");
+    console.log(`Example app listening on ${port}`);
 });
