efa1bd6e59850014015a7096269a7e2568ce726c
Jong Wook Kang <50934118+timbergrizz@users.noreply.github.com>
2022-06-08 02:44:51 +0900

    Feature : get data on fe from be, fix cors problem

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ const app = require("express")()
 const port = 3003
 const getConnection = require("./src/config/db_connection.js");
 const route = require("./src/route.js")
+const cors = require("cors")
 
+app.use(cors());
 app.use("/", route)
 
 app.listen(port, ()=>{
